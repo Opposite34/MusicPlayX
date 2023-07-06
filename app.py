ef675243c4dfb0633cca22b140597d175bc1d3da
Ping <54463205+Opposite34@users.noreply.github.com>
2023-07-06 13:03:54 +0900

    cookies

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.secret_key = APP_SECRET_KEY
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = 'filesystem'
+app.config["SESSION_COOKIE_SAMESITE"] = 'None'
+app.config["SESSION_COOKIE_SECURE"] = 'True'
 Session(app)
 
 title = "MusicPlayX"
