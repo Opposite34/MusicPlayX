f13e3aac7963404c5bb7e21bb4b2e04551faaeb9
Ping <opposite.three.four@gmail.com>
2023-07-14 02:27:10 +0900

    fix play next bug 'cause flask session doesn't stay

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.secret_key = APP_SECRET_KEY
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = 'filesystem'
 app.config["SESSION_COOKIE_SAMESITE"] = 'None'
 app.config["SESSION_COOKIE_SECURE"] = 'True'
